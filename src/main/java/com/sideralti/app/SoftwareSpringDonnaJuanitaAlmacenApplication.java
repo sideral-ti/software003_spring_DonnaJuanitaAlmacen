7b7f233d644ae481a9e06ed496d9615400a421ad
Sideral TI <sideralti.app@gmail.com>
2025-01-23 13:28:49 -0300

    initial project software

---
+++ src/main/java/com/sideralti/app/SoftwareSpringDonnaJuanitaAlmacenApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringDonnaJuanitaAlmacenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringDonnaJuanitaAlmacenApplication.class, args);
+	}
+
+}
